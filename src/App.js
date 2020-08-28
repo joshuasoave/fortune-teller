3591ab154b9127d433dab0d456ea55f0d282f485
Joshua Soave <joshua.soave@gmail.comgit config --global user.name JoshuaSoave>
2020-08-28 14:12:27 -0400

    can now generate three random cards with names cards and description

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header.js';
 
 class App extends React.Component {
   state = {
-
+    dealtCards: []
   }
 
   //Call 3rd party API for cards
@@ -13,6 +13,11 @@ class App extends React.Component {
       "https://tarot.howlcode.com/api/v1/spreads/three_cards"
     ).then((response) => {
       console.log(response.data);
+      this.setState(
+        {
+          dealtCards: response.data
+        }
+      )
     })
   }
 
@@ -22,6 +27,19 @@ class App extends React.Component {
         <Header/>
         <main>
           <button onClick={this.handleDealtCards}>Click</button>
+          <div>
+            {
+              this.state.dealtCards.map(
+                (card, index) => {
+                  return<div key={index}>
+                    <h2>{card.name}</h2>
+                    <img src={card.image}/>
+                    <p>{card.summary}</p>
+                  </div>
+                }
+              )
+            }
+          </div>
         </main>
       </div>
     )
