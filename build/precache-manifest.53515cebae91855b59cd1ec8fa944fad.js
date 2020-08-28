65afc4fa39b48f601fb2e5a25c2329ef5fc42028
Joshua Soave <joshua.soave@gmail.comgit config --global user.name JoshuaSoave>
2020-08-28 17:14:41 -0400

    updates to package json

---
+++ build/precache-manifest.53515cebae91855b59cd1ec8fa944fad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93c764c89900376f01dcfdf173546f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01103e94bb430c2ff089",
+    "url": "/static/css/main.fc3af2d9.chunk.css"
+  },
+  {
+    "revision": "6e3fd8ed9101cb69a0be",
+    "url": "/static/js/2.40791c26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.40791c26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01103e94bb430c2ff089",
+    "url": "/static/js/main.19563702.chunk.js"
+  },
+  {
+    "revision": "29505f8c2e14e46ac59b",
+    "url": "/static/js/runtime-main.6f63c765.js"
+  }
+]);
